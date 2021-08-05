8828be7dbaaee1a23accf805c7358bf0887256ca
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2021-08-04 21:37:10 -0300

    Add depth feature

--- mineChunk.lua
+++ mineChunk.lua
@@ -1,9 +1,22 @@
 turnLeft = true
+write("Depth: ")
+depth = tonumber(io.read())
 function blockAction()
   turtle.dig()
   turtle.forward()
   turtle.digDown()
 end
+function backStart()
+    turtle.turnRight()
+    i=0
+    while i< 16 do
+        i = i + 1
+        turtle.dig()
+        turtle.forward()
+    end
+    turtle.turnLeft()
+    turnLeft = true
+end 
 function turn()
     if(turnLeft) then
       turtle.dig()
@@ -23,14 +36,23 @@ function turn()
       turnLeft = true
    end
 end
+y=0
 i=0
 j=0
-while i < 16 do
-  while j < 16 do
-    blockAction()
-    j = j + 1
+while y < depth do 
+  while i < 16 do
+   while j < 16 do
+     blockAction()
+     j = j + 1
    end
   j=0
   i = i + 1
   turn()
+ end
+ backStart()
+ turtle.digDown()
+ turtle.down()
+ i=0
+ j=0
+ y=y+1
 end
