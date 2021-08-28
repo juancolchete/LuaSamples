2581bf562304cbc48ade394b5f8497f5b51e9dae
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2021-08-28 12:05:33 -0300

    Create roofPlacer.lua

---
+++ roofPlacer.lua
@@ -0,0 +1,51 @@
+print("Roof placer program")
+print("by juanudk")
+write("lenght:")
+lenght = tonumber(io.read())
+write("width:")
+width = tonumber(io.read())
+turnLeft = true
+blockCounter = 0
+grid = 1
+function place()
+    turtle.digUp()
+    turtle.placeUp()
+    blockCounter = blockCounter + 1
+    if(blockCounter == 64) then
+        grid = grid + 1
+        turtle.select(grid)
+        blockCounter = 0
+    end
+end
+function moveFoward()
+    turtle.dig()
+    turtle.forward()
+end
+function turn()
+    if(turnLeft) then
+      turtle.turnLeft()
+      moveFoward()
+      turtle.turnLeft()
+      moveFoward()
+      turnLeft = false    
+    else
+      turtle.turnRight()
+      moveFoward()
+      turtle.turnRight()
+      moveFoward()
+      turnLeft= true
+    end
+end
+ 
+i=0
+j=0
+while(j < width) do
+  while(i < lenght) do
+    i = i + 1
+    place()
+    moveFoward()
+  end
+  i=0
+  j = j + 1
+  turn()
+end
