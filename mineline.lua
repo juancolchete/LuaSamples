5a27ec1b4065561c60e2ee501268cd4a0b5fa6f8
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2023-04-15 09:22:40 -0300

    feat: add one line mineration

---
+++ mineline.lua
@@ -0,0 +1,34 @@
+write("Mine distance: ")
+size = tonumber(io.read())
+i=0
+turnCount=0
+torchCount=0
+turnDirection=0
+function putTorch()
+    turtle.select(16)
+    turtle.back()
+    turtle.placeUp()
+    turtle.forward()
+end
+function placeBlockDown()
+    turtle.select(1)
+    turtle.placeDown()
+end
+function mine()
+    turtle.dig()
+    turtle.forward()
+    turtle.digUp()
+end
+while i<size do
+    if(torchCount == 7)then
+        torchCount=0
+        putTorch()
+    end
+    if(turtle.detectDown() == false) then
+        placeBlockDown()
+    end
+    torchCount = torchCount + 1
+    turnCount = turnCount + 1
+    mine()
+    i = i + 1
+end
