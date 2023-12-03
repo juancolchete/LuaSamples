8a443ea6aae4cc7529b129679f96654913ade66e
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2023-12-03 10:29:10 -0300

    fix: get facing

--- getFacing.lua
+++ getFacing.lua
@@ -1,55 +1,20 @@
-local checkObs = true
-
-if checkObs then
-  for i=1,4 do
-    if turtle.detect() then
+function axis()
+    x1 , y1, z1 = gps.locate( 2, false )
+    while turtle.detect() do
       turtle.dig()
+        end
+        turtle.forward()
+    x2 , y2 , z3=gps.locate( 2, false )
+    if x2-x1 == 1 and y2-y1 ==0 then
+      dir="X"
+    elseif x2-x1 == -1 and y2-y1 ==0 then
+      dir="-X"
+    elseif x2-x1 == 0 and y2-y1 ==1 then
+      dir="Y"
+    elseif x2-x1 == 0 and y2-y1 ==-1 then
+      dir="-Y"
+    else
+      dir="something went terrible wrong"
     end
-    turtle.turnRight()
-  end
-  turtle.digUp()
-  turtle.digDown()
-end
-
-if type(turtle.getFuelLevel()) == "number" and turtle.getFuelLevel() < 2 then
-  print("Not enough Fuel")
-  return
-end
-
-local torchesM = {"West","East","North","South","Invalid"}
-
-local items = {["minecraft:ladder"]={"Invalid","South","North","East","West"},
-               ["minecraft:torch"]=torchesM,
-               ["minecraft:redstone_torch"]=torchesM}
-
-for i=1,16 do
-  if turtle.getItemCount(i) > 0 and not items[turtle.getItemDetail(i).name] then
-    turtle.select(i)
-    if turtle.place() then
-      break
-	end
-  end
-end
-
-repeat until turtle.up()
-
-for i=1,16 do
-  if turtle.getItemCount(i) > 0 and items[turtle.getItemDetail(i).name] then
-    turtle.select(i)
-    turtle.placeDown()
-    break
-  end
-end
-
-local d,b = turtle.inspectDown()
-
-turtle.digDown()
-repeat until turtle.down()
-turtle.dig()
-
-if not d then
-  print("That did not work")
-  return
-end
-
-print(string.format("This turtle is facing %s",items[b.name][b.metadata]))
+    return(dir)
+    end
