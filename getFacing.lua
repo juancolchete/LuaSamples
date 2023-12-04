79bb092bcf40d57a6d31a66fca586e96582d612e
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2023-12-03 23:03:32 -0300

    feat: when getting facing break block in front of

--- getFacing.lua
+++ getFacing.lua
@@ -4,6 +4,8 @@ local x, y, z = gps.locate( 1 )
 if not x then
   error( "No GPS available", 0 )
 end
+turtle.dig()
+turtle.digUp()
 if turtle.forward() then
   local nx, ny, nz = gps.locate( 1 )
   if x - nx == 1 then
