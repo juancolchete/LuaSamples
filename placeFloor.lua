5a448c2f9c22ddf8423319ad3238190d65829222
juancolchete <38388534+juancolchete@users.noreply.github.com>
2024-02-04 13:48:06 -0300

    feat!: do not construct over 2024

--- placeFloor.lua
+++ placeFloor.lua
@@ -4,6 +4,7 @@ local depth = tonumber(args[1])
 i=0
 floorCount=0
 selected=0
+coeficient=1
 function turn()
     if(turnDirection == 0)then
         turnDirection = 1
@@ -25,11 +26,79 @@ function placeFloor()
     turtle.forward()
     floorCount = floorCount + 1
 end
+function goBack(count) 
+    for i=1,count do
+        turtle.back()
+    end
+end
+function goForward(count) 
+    for i=1,count do
+        turtle.forward()
+    end
+end
+function storeAndRefillBlocks()
+    for i=8,16 do
+        turtle.select(i)
+        turtle.drop()
+    end
+    refillBlocks()
+    floorCount = 0
+end
+function refillBlocks()
+    for i=1,8 do
+        turtle.select(i)
+        turtle.suck()
+    end
+end
+function returnToPlaceBlock(w,d)
+    turtle.turnLeft()
+    turtle.turnLeft()
+    flag = true
+    for i=1, d do
+        goForward(w-2)
+        if(d > 1) then
+            if(flag == true) then
+                turtle.turnRight()
+                placeFloor()
+                turtle.turnRight()
+                flag = false
+            else
+                turtle.turnLeft()
+                placeFloor()
+                turtle.turnLeft()
+                flag = true
+            end
+        end
+    end
+end
 function run()
+    turtle.turnLeft()
+    turtle.turnLeft()
+    refillBlocks()
+    turtle.turnRight()
+    turtle.turnRight()
     up = true
     right = true
     for d=1,depth do
         for w=2,width do
+            if(d*w >= 512*coeficient) then
+                coeficient = coeficient + 1
+                if(right == true) then
+                    turtle.turnRight()
+                    goBack(d)
+                    turtle.turnLeft()
+                    goBack(w)
+                else
+                    turtle.turnLeft()
+                    goBack(d)
+                    turtle.turnLeft()
+                    goBack(w)
+                end
+                turtle.turnLeft()
+                turtle.turnLeft()
+                storeAndRefillBlocks()
+                returnToPlaceBlock(w,d)
+            end
             placeFloor()
         end
         if(right == true) then
